4936b1e62e9a3a9e1e4c7617593e7f81d68e3595
José Alcir Barbosa de Sousa <jose.alcir@boavista.local>
2025-05-09 12:59:57 -0300

    Nova automação e rodando automações

--- Selenium/testeselenium.py
+++ Selenium/testeselenium.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 #abrir navegador
@@ -8,7 +13,7 @@ navegador = webdriver.Chrome()
 navegador.get('https://www.eextrato.com.br/conciliador/pages/painelControleArquivos/painelControleArquivos.xhtml')
 navegador.maximize_window()
 
-#selecionar elemento
+#Realizando login
 login = navegador.find_element("name", "user")
 
 login.click()
@@ -22,4 +27,55 @@ password.send_keys("Alcir@2022")
 acessar = navegador.find_element("id","entrar")
 acessar.click()
 
-time.sleep(10)
+#Painel Controle de arquivos
+navegador.get('https://www.eextrato.com.br/conciliador/pages/painelControleArquivos/painelControleArquivos.xhtml#')
+
+#Grupo empresa
+g_empresa = navegador.find_element("id", "formulario:grupoEmpresa_label")
+g_empresa.click()
+time.sleep(2)
+g_empresa = navegador.find_element("id", "formulario:grupoEmpresa_filter")
+g_empresa.send_keys("Todos")
+time.sleep(1)
+g_empresa.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+#Empresa
+empresa = navegador.find_element("id","formulario:comboEmpresa_label")
+empresa.click()
+time.sleep(5)
+empresa = navegador.find_element("id","formulario:comboEmpresa_filter")
+empresa.send_keys("Selecione")
+time.sleep(2)
+empresa.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+#Pesquisar
+pesquisar = navegador.find_element("id","formulario:j_idt1792")
+pesquisar.click()
+
+#pegando operadoras zeradas
+
+time.sleep(20)
+
+linhas = navegador.find_elements(By.XPATH, "//tbody[@id='formulario:tblData_data']/tr")
+operadoras_sem_arquivos = []
+
+for linha in linhas:
+    colunas = linha.find_elements(By.TAG_NAME, "td")
+    
+    if not colunas:
+        continue
+
+    nome_operadora = colunas[0].text.strip()
+    dados = [col.text.strip() for col in colunas[1:]]
+
+    # Verifica se todos os dados seguintes estão vazios ou zero
+    if all(dado in ("", "0") for dado in dados):
+        operadoras_sem_arquivos.append(nome_operadora)
+
+print("Operadoras sem arquivos:", operadoras_sem_arquivos)
+
+time.sleep(100) 
